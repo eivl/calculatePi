c25be8829c5707755810826fa08d57a84e5aa15d
Eivind Teig <eivind.teig@gmail.com>
2017-03-20 14:56:50 +0100

    added calculation of pi

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
-from math import gcd
+from math import gcd, sqrt, pi
+from statistics import mean
 import random
 
-UPPER_BOUND = 120
+UPPER_BOUND = 1000
 ITTERATIONS = 500
+RUN_NUMBER_OF_TIMES = 10
 
 
 def two_numbers():
     yield (random.randint(0, UPPER_BOUND), random.randint(0, UPPER_BOUND))
 
-for i in range(ITTERATIONS):
-    v = next(two_numbers())
-    gcdTest = gcd(v[0], v[0])
-    if(gcdTest == 1):
-        print("{} \tand \t{} \tare CoPrime with a factor of {}"
-              .format(v[0], v[1], gcdTest))
-    else:
-        print("{} \tand \t{} \tare CoFactor with a factor of {}"
-              .format(v[0], v[1], gcdTest))
+
+def calculatePi():
+    coprime = 0
+    cofactor = 0
+    for i in range(ITTERATIONS):
+        twoNumbers = next(two_numbers())
+        gcdTest = gcd(twoNumbers[0], twoNumbers[1])
+        if(gcdTest == 1):
+            coprime += 1
+        else:
+            cofactor += 1
+    return (sqrt(6/(coprime/ITTERATIONS)))
+
+piray = [calculatePi() for _ in range(RUN_NUMBER_OF_TIMES)]
+calculatedPi = mean(piray)
+piError = calculatedPi/pi
+
+if(piError > 1):
+    p = (piError-1)/100
+    print("The calculated value of Pi is {:.6f},"
+          "this is {:.6f}% above the correct value".format(calculatedPi, p))
+elif(piError < 1):
+    p = (1-piError)/100
+    print("The calculated value of Pi is {:.6f},"
+          "this is {:.6f}% below the correct value".format(calculatedPi, p))
+else:
+    print("Calculated Pi is exacly eaqual to Pi")
