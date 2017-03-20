71e96c8c8c8c5a827d5b04c67201425a959a2272
Eivind Teig <eivind.teig@gmail.com>
2017-03-20 15:13:47 +0100

    added comments

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ ITTERATIONS = 500  # the number of itterations when calculating Pi
 RUN_NUMBER_OF_TIMES = 10  # the number of times the caluclation of Pi
 
 
-#
+# funcion to yield two random numbers
 def two_numbers():
     yield (random.randint(0, UPPER_BOUND), random.randint(0, UPPER_BOUND))
 
 
+# calculate pi based on the ration of coprimes and the number of itterations
 def calculatePi():
     coprime = 0
-    cofactor = 0
+    cofactor = 0  # the cofactor is not actually needed for anything.
     for i in range(ITTERATIONS):
         twoNumbers = next(two_numbers())
         gcdTest = gcd(twoNumbers[0], twoNumbers[1])
@@ -24,17 +25,20 @@ def calculatePi():
             cofactor += 1
     return (sqrt(6/(coprime/ITTERATIONS)))
 
+# make an array of pi calculations that is RUN_NUMBER_OF_TIMES long
 piray = [calculatePi() for _ in range(RUN_NUMBER_OF_TIMES)]
-calculatedPi = mean(piray)
-piError = calculatedPi/pi
+calculatedPi = mean(piray)  # mean for the array
+piError = calculatedPi/pi  # error ratio with the real pi
 
+# output result based on if the calculation is above or below real pi.
+# numbers are trunchated to 6 decimal places.
 if(piError > 1):
     p = (piError-1)*100
     print("The calculated value of Pi is {:.6f},"
-          "this is {:.6f}% above the correct value".format(calculatedPi, p))
+          "this is {:.6f} % above the correct value".format(calculatedPi, p))
 elif(piError < 1):
     p = (1-piError)*100
     print("The calculated value of Pi is {:.6f},"
-          "this is {:.6f}% below the correct value".format(calculatedPi, p))
+          "this is {:.6f} % below the correct value".format(calculatedPi, p))
 else:
-    print("Calculated Pi is exacly eaqual to Pi")
+    print("Calculated Pi is exacly eaqual to Pi")  # GLHF
